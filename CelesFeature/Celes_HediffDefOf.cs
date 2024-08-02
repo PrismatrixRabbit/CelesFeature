7809635d488802faf8c07d624b128a698543adcb
Prismatrix_Rabbit <969870910@qq.com>
2024-08-02 15:03:39 +0800

    111111

--- CelesFeature/Celes_HediffDefOf.cs
+++ CelesFeature/Celes_HediffDefOf.cs
@@ -7,5 +7,14 @@ namespace CelesFeature
     public static class Celes_HediffDefOf
     {
         public static HediffDef MechlinkImplant;
+        public static HediffDef GutWorms;
+        public static HediffDef MuscleParasites;
+        public static HediffDef FibrousMechanites;
+        public static HediffDef SensoryMechanites;
+        public static HediffDef WoundInfection;
+        public static HediffDef Flu;
+        public static HediffDef Celes_NanoImplanted_I;
+        public static HediffDef Celes_NanoImplanted_II;
+        public static HediffDef Celes_NanoImplanted_III;
     }
 }
---
+++ CelesFeature/Celes_Recipe_Surgery.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace CelesFeature
+{
+    public class Celes_Recipe_Surgery_AddHediff:Recipe_Surgery
+    {
+        public override bool AvailableOnNow(Thing thing, BodyPartRecord part = null)
+        {
+            if (!base.AvailableOnNow(thing, part))
+            {
+                return false;
+            }
+            if (!(thing is Pawn pawn))
+            {
+                return false;
+            }
+            if (pawn.health.hediffSet.HasHediff(Celes_HediffDefOf.Celes_NanoImplanted_II) || pawn.health.hediffSet.HasHediff(Celes_HediffDefOf.Celes_NanoImplanted_I)||pawn.health.hediffSet.HasHediff(Celes_HediffDefOf.Celes_NanoImplanted_III))
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+}

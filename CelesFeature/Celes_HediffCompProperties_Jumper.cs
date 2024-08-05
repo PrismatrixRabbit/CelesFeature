ee771134078364bc58370d2b0c180f230c459374
Prismatrix_Rabbit <969870910@qq.com>
2024-08-05 12:49:01 +0800

    wcnm

--- CelesFeature/Celes_HediffCompProperties_Jumper.cs
+++ CelesFeature/Celes_HediffCompProperties_Jumper.cs
@@ -5,7 +5,8 @@ namespace CelesFeature
         public class Celes_HediffCompProperties_Jumper : HediffCompProperties
         {
             public HediffDef targetHediff;
-
+            public float chanceToJump=1f;
+            public bool minToJump=false;
             public Celes_HediffCompProperties_Jumper()
             {
                 compClass = typeof(Celes_HediffComp_Jumper);
--- CelesFeature/Celes_HediffComp_Jumper.cs
+++ CelesFeature/Celes_HediffComp_Jumper.cs
@@ -7,12 +7,32 @@ namespace CelesFeature
     public class Celes_HediffComp_Jumper : HediffComp
     {
         public Celes_HediffCompProperties_Jumper Props => (Celes_HediffCompProperties_Jumper)props;
+        public float rand;
         public override void CompPostTick(ref float severityAdjustment)
         {
-            if (this.parent.Severity == this.parent.def.maxSeverity)
+            if(!Props.minToJump)
             {
-                Pawn.health.AddHediff(Props.targetHediff);
-                Pawn.health.RemoveHediff(parent);
+                rand = Rand.Range(0f, 1f);
+                if(rand<=Props.chanceToJump)
+                {
+                    if (this.parent.Severity == this.parent.def.maxSeverity)
+                    {
+                        Pawn.health.RemoveHediff(parent);
+                        Pawn.health.AddHediff(Props.targetHediff);
+                    }
+                }
+            }
+            else if (Props.minToJump)
+            {
+                rand = Rand.Range(0f, 1f);
+                if(rand<=Props.chanceToJump)
+                {
+                    if (this.parent.Severity == this.parent.def.minSeverity)
+                    {
+                        Pawn.health.RemoveHediff(parent);
+                        Pawn.health.AddHediff(Props.targetHediff);
+                    }
+                }
             }
         }
     }

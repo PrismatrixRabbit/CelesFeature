3069607aa43f9d738c5e25bb791915546fb75eac
Prismatrix_Rabbi <969870910@qq.com>
2025-02-27 19:50:38 +0800

    二段伤害试作型

---
+++ CelesFeature/Celes_DamageDefOf.cs
@@ -0,0 +1,12 @@
+using RimWorld;
+using Verse;
+
+namespace CelesFeature
+{
+    [DefOf]
+    public static class Celes_DamageDefOf
+    {
+        public static DamageDef Celes_Particle;
+        public static DamageDef Celes_MassiveParticle;
+    }
+}

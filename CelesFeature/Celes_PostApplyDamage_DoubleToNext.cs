538d539c5fc0580adbaa8c0ffe1071ca1b179731
Prismatrix_Rabbit <969870910@qq.com>
2025-03-01 21:01:35 +0800

    Pawn_HealthTracker->Pawn

--- CelesFeature/Celes_PostApplyDamage_DoubleToNext.cs
+++ CelesFeature/Celes_PostApplyDamage_DoubleToNext.cs
@@ -8,15 +8,13 @@ namespace CelesFeature
     {
         public float multiplier = 0.5f; 
     }
-    [HarmonyPatch(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.PostApplyDamage))]
+    [HarmonyPatch(typeof(Pawn), nameof(Pawn.PostApplyDamage))]
     public static class Celes_PostApplyDamage_DoubleToNext
     {
-        private static readonly FieldInfo PawnField = AccessTools.Field(typeof(Pawn_HealthTracker), "pawn");
-
-        public static void Postfix(Pawn_HealthTracker __instance, DamageInfo dinfo, float totalDamageDealt)
+        
+        public static void Postfix(Pawn __instance, DamageInfo dinfo, float totalDamageDealt)
         {
-            Pawn pawn=PawnField.GetValue(__instance) as Pawn;
-            if(pawn==null || pawn.Dead)return;
+            if(__instance==null || __instance.Dead)return;
             if(dinfo.Def!=Celes_DamageDefOf.Celes_Particle && dinfo.Def!=Celes_DamageDefOf.Celes_MassiveParticle)return;
             if(dinfo.HitPart==null)return;
 
@@ -27,7 +25,7 @@ namespace CelesFeature
                 DamageInfo extraDamage = new DamageInfo(dinfo);
                 extraDamage.SetAmount(damageAmount);
                 extraDamage.Def = Celes_DamageDefOf.Burn;
-                pawn.TakeDamage(extraDamage);
+                __instance.TakeDamage(extraDamage);
             }
             
         }

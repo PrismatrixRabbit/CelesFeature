f1ab23fe7412fe5a345ca32e05081adbd25eb685
Prismatrix_Rabbit <969870910@qq.com>
2025-03-01 19:00:27 +0800

    二段伤害第三次制作 补药再花钱辣

---
+++ CelesFeature/Celes_PostApplyDamage_DoubleToNext.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+namespace CelesFeature
+{
+    public class Celes_DamageExtension_Multiplier : DefModExtension
+    {
+        public float multiplier = 0.5f; 
+    }
+    [HarmonyPatch(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.PostApplyDamage))]
+    public static class Celes_PostApplyDamage_DoubleToNext
+    {
+        private static readonly FieldInfo PawnField = AccessTools.Field(typeof(Pawn_HealthTracker), "pawn");
+
+        public static void Postfix(Pawn_HealthTracker __instance, DamageInfo dinfo, float totalDamageDealt)
+        {
+            Pawn pawn=PawnField.GetValue(__instance) as Pawn;
+            if(pawn==null || pawn.Dead)return;
+            if(dinfo.Def!=Celes_DamageDefOf.Celes_Particle && dinfo.Def!=Celes_DamageDefOf.Celes_MassiveParticle)return;
+            if(dinfo.HitPart==null)return;
+
+            if (dinfo.Def.GetModExtension<Celes_DamageExtension_Multiplier>()
+                is Celes_DamageExtension_Multiplier ext)
+            {
+                DamageInfo extraDamage = new DamageInfo(dinfo);
+                extraDamage.SetAmount(totalDamageDealt * ext.multiplier);
+                extraDamage.Def = Celes_DamageDefOf.Burn;
+                pawn.TakeDamage(extraDamage);
+            }
+            
+        }
+    }
+}

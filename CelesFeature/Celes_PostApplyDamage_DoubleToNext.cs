9c2039fb069d36e2ecad853402bcb6ec40790e5b
Prismatrix_Rabbit <969870910@qq.com>
2025-03-01 21:16:52 +0800

    1

--- CelesFeature/Celes_PostApplyDamage_DoubleToNext.cs
+++ CelesFeature/Celes_PostApplyDamage_DoubleToNext.cs
@@ -14,13 +14,16 @@ namespace CelesFeature
         
         public static void Postfix(Pawn __instance, DamageInfo dinfo, float totalDamageDealt)
         {
+            Harmony.DEBUG = true;
+            FileLog.Reset();
+            FileLog.Log("初始化段");
             if(__instance==null || __instance.Dead)return;
             if(dinfo.Def!=Celes_DamageDefOf.Celes_Particle && dinfo.Def!=Celes_DamageDefOf.Celes_MassiveParticle)return;
             if(dinfo.HitPart==null)return;
-
             if (dinfo.Def.GetModExtension<Celes_DamageExtension_Multiplier>()
                 is Celes_DamageExtension_Multiplier ext)
             {
+                FileLog.Log("作用段");
                 float damageAmount = totalDamageDealt * ext.multiplier;
                 DamageInfo extraDamage = new DamageInfo(dinfo);
                 extraDamage.SetAmount(damageAmount);

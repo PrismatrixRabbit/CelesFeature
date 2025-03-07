2f2469b5932e814c43f461b19a52a96ec88e5b90
Prismatrix_Rabbi <969870910@qq.com>
2025-03-07 12:05:50 +0800

    添加新的recipeDef的workerClass

---
+++ CelesFeature/Celes_RecipeSurgery.cs
@@ -0,0 +1,112 @@
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace CelesFeature
+{
+    public class Celes_RecipeModExtension : DefModExtension
+    {
+        public List<HediffDef> listA;
+        public List<HediffDef> listB;
+        public bool useRace=false;
+        public List<PawnKindDef> race=null;
+        public bool addHediff;
+        public bool removeHediff;
+        public HediffDef hediffToAdd;
+        public HediffDef hediffToRemove;
+    }
+    public class Celes_RecipeSurgery:Recipe_Surgery
+    {
+        private bool TriggerA;
+        private bool TriggerB;
+        public override bool AvailableOnNow(Thing thing, BodyPartRecord part=null)
+        {
+            return IsValidNow(thing, part);
+        }
+        public bool IsValidNow(Thing thing, BodyPartRecord part = null, bool ignoreBills = false)
+        {
+            if (!base.AvailableOnNow(thing, part))
+            {
+                return false;
+            }
+            if (!(thing is Pawn pawn))
+            {
+                return false;
+            }
+            if (part != null && (pawn.health.WouldDieAfterAddingHediff(recipe.addsHediff, part, 1f) || pawn.health.WouldLosePartAfterAddingHediff(recipe.addsHediff, part, 1f)))
+            {
+                return false;
+            }
+
+            if (!recipe.HasModExtension<Celes_RecipeModExtension>())
+            {
+                return false;
+            }
+            
+            if (!ignoreBills && pawn.BillStack.Bills.Any((Bill b) => b.recipe == recipe))
+            {
+                return false;
+            }
+            
+            Celes_RecipeModExtension ext = recipe.GetModExtension<Celes_RecipeModExtension>();
+            foreach (HediffDef def in ext.listB)
+            {
+                if (pawn.health.hediffSet.HasHediff(def))
+                {
+                    return false;
+                }
+            }
+
+            foreach (HediffDef def in ext.listA)
+            {
+                if (!pawn.health.hediffSet.HasHediff(def))
+                {
+                    return false;
+                }
+            }
+
+            if (ext.useRace)
+            {
+                foreach (PawnKindDef def in ext.race)
+                {
+                    if (pawn.kindDef != def)
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            if (pawn.health.hediffSet.hediffs.Any((Hediff x) => !recipe.CompatibleWithHediff(x.def)))
+            {
+                return false;
+            }
+            return true;
+        }
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            if (billDoer != null)
+            {
+                if (CheckSurgeryFail(billDoer, pawn, ingredients, part, bill))
+                {
+                    return;
+                }
+                TaleRecorder.RecordTale(TaleDefOf.DidSurgery, billDoer, pawn);
+            }
+            Celes_RecipeModExtension ext = recipe.GetModExtension<Celes_RecipeModExtension>();
+            if (ext.addHediff)
+            {
+                pawn.health.AddHediff(ext.hediffToAdd, part);
+            }
+
+            if (ext.removeHediff)
+            {
+                pawn.health.RemoveHediff(pawn.health.hediffSet.GetFirstHediffOfDef(ext.hediffToRemove));
+            }
+            OnSurgerySuccess(pawn, part, billDoer, ingredients, bill);
+            if (IsViolationOnPawn(pawn, part, Faction.OfPlayerSilentFail))
+            {
+                ReportViolation(pawn, billDoer, pawn.HomeFaction, -70);
+            }
+        }
+    }
+}
